1f546a358ee9e575aa5ac7a1eed3dc4ad1ac8820
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2022-12-05 02:27:19 +0100

    Again

--- examples/style.php
+++
@@ -1,7 +0,0 @@
-<?php
-use Codad5\PhpInex\Import;
-
-$export = Import::this('index.css');
-$me0n = 900;
-echo "hello";
-
--- src/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Codad5\PhpInex;
-
-require __DIR__ . '../vendor/autoload.php';
-
-include __DIR__ . '/Import.php';
